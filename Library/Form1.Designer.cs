176107fb02a4f8e1072f9e6c9198f612b57a4cca
null <16jesu@stockholmscience.se>
2019-09-06 16:15:56 +0200

    First commit

---
+++ Library/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Library
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lstBooks = new System.Windows.Forms.ListBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtBook = new System.Windows.Forms.TextBox();
+            this.txtAuthor = new System.Windows.Forms.TextBox();
+            this.lblBookName = new System.Windows.Forms.Label();
+            this.lblAuthorName = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnBorrow = new System.Windows.Forms.Button();
+            this.btnReturn = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnReturn);
+            this.groupBox1.Controls.Add(this.btnBorrow);
+            this.groupBox1.Controls.Add(this.btnSearch);
+            this.groupBox1.Controls.Add(this.txtSearch);
+            this.groupBox1.Controls.Add(this.lstBooks);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(218, 233);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Books";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnRemove);
+            this.groupBox2.Controls.Add(this.btnAdd);
+            this.groupBox2.Controls.Add(this.lblAuthorName);
+            this.groupBox2.Controls.Add(this.lblBookName);
+            this.groupBox2.Controls.Add(this.txtAuthor);
+            this.groupBox2.Controls.Add(this.txtBook);
+            this.groupBox2.Location = new System.Drawing.Point(236, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(218, 233);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Add a book";
+            // 
+            // lstBooks
+            // 
+            this.lstBooks.FormattingEnabled = true;
+            this.lstBooks.Location = new System.Drawing.Point(6, 54);
+            this.lstBooks.Name = "lstBooks";
+            this.lstBooks.Size = new System.Drawing.Size(206, 147);
+            this.lstBooks.TabIndex = 0;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(6, 25);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(147, 20);
+            this.txtSearch.TabIndex = 1;
+            this.txtSearch.Enter += new System.EventHandler(this.txtSearch_Enter);
+            this.txtSearch.Leave += new System.EventHandler(this.txtSearch_Leave);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(159, 23);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(53, 23);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtBook
+            // 
+            this.txtBook.Location = new System.Drawing.Point(11, 39);
+            this.txtBook.Name = "txtBook";
+            this.txtBook.Size = new System.Drawing.Size(154, 20);
+            this.txtBook.TabIndex = 0;
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.Location = new System.Drawing.Point(11, 84);
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.Size = new System.Drawing.Size(154, 20);
+            this.txtAuthor.TabIndex = 1;
+            // 
+            // lblBookName
+            // 
+            this.lblBookName.AutoSize = true;
+            this.lblBookName.Location = new System.Drawing.Point(8, 23);
+            this.lblBookName.Name = "lblBookName";
+            this.lblBookName.Size = new System.Drawing.Size(64, 13);
+            this.lblBookName.TabIndex = 2;
+            this.lblBookName.Text = "Book name:";
+            // 
+            // lblAuthorName
+            // 
+            this.lblAuthorName.AutoSize = true;
+            this.lblAuthorName.Location = new System.Drawing.Point(8, 68);
+            this.lblAuthorName.Name = "lblAuthorName";
+            this.lblAuthorName.Size = new System.Drawing.Size(41, 13);
+            this.lblAuthorName.TabIndex = 3;
+            this.lblAuthorName.Text = "Author:";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(11, 110);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(11, 204);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 5;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnBorrow
+            // 
+            this.btnBorrow.Location = new System.Drawing.Point(6, 204);
+            this.btnBorrow.Name = "btnBorrow";
+            this.btnBorrow.Size = new System.Drawing.Size(75, 23);
+            this.btnBorrow.TabIndex = 3;
+            this.btnBorrow.Text = "Borrow";
+            this.btnBorrow.UseVisualStyleBackColor = true;
+            this.btnBorrow.Click += new System.EventHandler(this.btnBorrow_Click);
+            // 
+            // btnReturn
+            // 
+            this.btnReturn.Location = new System.Drawing.Point(137, 204);
+            this.btnReturn.Name = "btnReturn";
+            this.btnReturn.Size = new System.Drawing.Size(75, 23);
+            this.btnReturn.TabIndex = 4;
+            this.btnReturn.Text = "Return";
+            this.btnReturn.UseVisualStyleBackColor = true;
+            this.btnReturn.Click += new System.EventHandler(this.btnReturn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 255);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.ListBox lstBooks;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label lblAuthorName;
+        private System.Windows.Forms.Label lblBookName;
+        private System.Windows.Forms.TextBox txtAuthor;
+        private System.Windows.Forms.TextBox txtBook;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnReturn;
+        private System.Windows.Forms.Button btnBorrow;
+    }
+}
+
---
+++ Library/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Library
+{
+    public partial class Form1 : Form
+    {
+        private string Path = @"C:\untitled\books.txt";
+
+        public BindingList<string> Books;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Books = new BindingList<string>(File.ReadLines(Path).OrderBy(x => x[0]).ToList());
+
+            txtSearch.Text = "Search a book...";
+            txtSearch.ForeColor = Color.Gray;
+
+            lstBooks.DataSource = Books;
+        }
+
+        private void txtSearch_Enter(object sender, EventArgs e)
+        {
+            txtSearch.ForeColor = Color.Black;
+
+            if (txtSearch.Text == "Search a book...")
+            {
+                txtSearch.Text = "";
+            }
+        }
+
+        private void txtSearch_Leave(object sender, EventArgs e)
+        {
+            if (txtSearch.Text == "")
+            {
+                txtSearch.Text = "Search a book...";
+                txtSearch.ForeColor = Color.Gray;
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Books.Add($"{txtBook.Text} - {txtAuthor.Text}");
+            File.WriteAllLines(Path, Books);
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (lstBooks.SelectedIndex == -1) return;
+
+            Books.Remove(lstBooks.SelectedItem.ToString());
+            File.WriteAllLines(Path, Books);
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            lstBooks.DataSource = (txtSearch.Text != "Search a book...") ? new BindingList<string>(Books.Where(x => x[0].ToString().ToLower().Contains(txtSearch.Text)).ToList()) : Books;    
+        }
+
+        private void btnBorrow_Click(object sender, EventArgs e)
+        {
+            string Text = File.ReadAllText(Path);
+
+            if (Text.Contains(lstBooks.SelectedItem.ToString()) && lstBooks.SelectedItem.ToString().Contains("*"))
+            {
+                MessageBox.Show("That book is already borrowed!");
+                return;
+            }
+            else
+            {
+                Text = Text.Replace(lstBooks.SelectedItem.ToString(), lstBooks.SelectedItem.ToString() + "*");
+                File.WriteAllText(Path, Text);
+
+                MessageBox.Show("That book is now borrowed!");
+
+                Books[lstBooks.SelectedIndex] = lstBooks.SelectedItem.ToString() + "*";
+            }
+        }
+
+        private void btnReturn_Click(object sender, EventArgs e)
+        {
+            string Text = File.ReadAllText(Path);
+
+            if (Text.Contains(lstBooks.SelectedItem.ToString()) && lstBooks.SelectedItem.ToString().Contains("*"))
+            {
+                Text = Text.Replace(lstBooks.SelectedItem.ToString(), lstBooks.SelectedItem.ToString().Replace("*", ""));
+                File.WriteAllText(Path, Text);
+
+                MessageBox.Show("That book is now returned!");
+
+                Books[lstBooks.SelectedIndex] = lstBooks.SelectedItem.ToString().Replace("*", "");
+            }
+            else
+            {
+                MessageBox.Show("That book is not borrowed.");
+            }
+        }
+    }
+}
